4b33ae8335f994956f26e8e235083a4ef087a50d
Emi43 <74046670+Emi43@users.noreply.github.com>
2021-05-05 17:20:29 +0200

    mise en place panier

--- js/produit.js
+++ js/produit.js
@@ -23,7 +23,7 @@ window.onload = getTeddy();
 function displayTeddy(response){
     document.getElementById("affich_product").innerHTML +=
         "<div>" +
-        "<h2>" +
+        "<h2>"  +
          response.name +
         "</h2>" +
         "<img width=100% src='" +
@@ -41,19 +41,36 @@ function displayTeddy(response){
         "</div>";
 
 //menu déroulant colors//
-    for (let i = 0; i < response.length; i++) {
-        document.getElementById('select').innerHTML +=
-        "<option value = '' + response.colors[i] + ''>" + "</option>"
-        console.log(option);
-    }
+ let selectOption = ""
+    for (let i = 0; i < response.colors.length; i++) {
+        selectOption +="<option value=\""+response.colors[i]+"\">"+response.colors[i]+"</option>";
+    }    
+        console.log(selectOption);
+        document.getElementById('select').innerHTML = selectOption
 
-        document.getElementById('button').addEventListener("click",function(){
-        //function addTeddyToBasket pour récuperer le panier et son contenu//
-            addTeddyToBasket();
+        document.getElementById('button').addEventListener("click", function(){
+        //function addProduct pour récuperer le panier et son contenu//
+            addProduct();
         document.getElementById('alerte_ajout_panier').innerHTML = "Le produit a été ajouté au panier";
     });
 };
 //localstorage//
-function addTeddyToBasket(){
-
-}
+function addProduct(){
+    //transformation la chaine JSON(string) en objet JS//
+    let card = JSON.parse(localStorage.getItem('card'));
+    //si le tableau n'existe pas je le crée//
+    if (card === null){
+        card = []
+    }
+    //les produits sélectionnés s'ajoutent//    
+        card.push(productId);
+        //transformation de l'objet JS en chaine de charactère//
+        localStorage.setItem("card",JSON.stringify(card));
+    
+};
+//sauvegarde la valeur JSON.stringify//
+let write = localStorage.getItem('card');
+console.log(write);
+//lit la valeur dans le localstorage//
+let read = JSON.parse(write);
+console.log(read);
