8dc32230368f90b613a76ee1b0811637b0a57b1c
Emi43 <74046670+Emi43@users.noreply.github.com>
2021-05-13 07:40:44 +0200

    vider le panier

--- js/panier.js
+++ js/panier.js
@@ -1,9 +1,19 @@
 let read = JSON.parse(localStorage.getItem("panier"));
 console.log(read);
 let article;
+
+//message pour informer que le panier est vide//
+if (read === null){
+    document.getElementById("panier_vide").innerHTML +="Votre panier est vide"
+    console.log("je suis vide");
+}
+else{
+    console.log("articles dans panier");
+}
+//affichage du contenu du panier//
 for (let article of read){
     document.getElementById("container_teddy_selectionne").innerHTML +=
-        "<div>" +
+        "<div id='teddy_selectionne'>" +
         "<h2>"  +
          article.name +
         "</h2>" +
@@ -17,3 +27,27 @@ for (let article of read){
         "</p>" +
         "</div>"
 }
+//...........bouton pour vider le panier.............//
+document.getElementById("vider_panier").addEventListener('click', function () {
+    //permet de supprimer tous les éléments du panier dans le localStorage
+    localStorage.removeItem("panier");
+    //Permet le rechargement de la page
+    document.location.reload(true);
+})
+//...........bouton pour vider le panier.............//
+
+//...........montant total du panier.............//
+//aller chercher les prix dans le panier//
+let prixTotalCalcul= [];
+for (k = 0; k < read.length; k++ ){
+    let prixProduitDansLePanier = read[k].price;
+    //mettre les prix du panier dans la variable prixTotalCalcul//
+    prixTotalCalcul.push(prixProduitDansLePanier)
+    console.log(prixTotalCalcul)
+}
+//additionner les prix du tableau de la variable"prixTotalCalcul"avec la methode reduce//
+const reducer = (accumulator,currentValue)=> accumulator + currentValue
+const prixTotal = prixTotalCalcul.reduce(reducer,0);
+console.log(prixTotal);
+//code html du prix à afficher//
+ document.getElementById("result_order").innerHTML = "TOTAL de la commande : " + prixTotal / 100 + " €" ;
